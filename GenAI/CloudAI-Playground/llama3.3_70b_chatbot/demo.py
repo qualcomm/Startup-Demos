5448aeb20a0d3babcb310265fbd9a6cfacff5b65
Ray Tung <rtung@qti.qualcomm.com>
2025-08-06 14:24:50 +0800

    Adding license information

--- GenAI/CloudAI-Playground/llama3.3_70b_chatbot/demo.py
+++ GenAI/CloudAI-Playground/llama3.3_70b_chatbot/demo.py
@@ -1,3 +1,11 @@
+#===--demo.py----------------------------------------------===//
+# Part of the Startup-Demos Project, under the MIT License
+# See https://github.com/qualcomm/Startup-Demos/blob/main/LICENSE.txt
+# for license information.
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: MIT License
+#===----------------------------------------------------------------------===//
+
 from typing import List, Optional, Union
 from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 import QEfficient
